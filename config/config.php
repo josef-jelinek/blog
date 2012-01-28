8f20ebc558a7b7302d25844acece227319493bb0
josef-jelinek <josef.jelinek@gmail.com>
2012-01-28 02:33:13 +0100

    cleanup

--- config/config.php
+++ config/config.php
@@ -1,9 +1,8 @@
 <?php
 if (!isset($out)) exit;
 $config = array(
-  'password' => '3f14b6841ab09dc36d32a3d6f80ade58',
-  //'password' => 'd932e735bff2c23aa835ffc70923147b',
-  'title' => 'Test Blog',
+  'password' => '3f14b6841ab09dc36d32a3d6f80ade58', // "test" without quotes
+  'title' => 'Blog Title',
   'maxCommentLength' => 10000,
   'maxNameLength' => 40,
 );
